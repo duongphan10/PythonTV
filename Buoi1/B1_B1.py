7b54585e1d48619d5530902b19a9cf148a1f3af2
PHAN <duongboygh10@gmail.com>
2021-10-20 07:41:57 +0700

    Add buoi1

---
+++ Buoi1/B1_B1.py
@@ -0,0 +1,5 @@
+import math
+
+a = float(input("Nhap ban kinh: "))
+print("Dien tich hinh tron: ", math.pi*a**2)
+print("Chu vi hinh tron: ", 2*a*math.pi)
---
+++ Buoi1/B1_B2.py
@@ -0,0 +1,4 @@
+import math
+x = float(input("Nhap x: "))
+f = (x**2 + math.e**x+ math.sin(x))/math.sqrt(x**2+1)
+print("f(x) = ",f)
