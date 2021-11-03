0bc3f46fddddcea4bdb059f2120423151334f618
PHAN <duongboygh10@gmail.com>
2021-11-03 23:19:23 +0700

    add buoi2

--- Buoi2/B4.py
+++ Buoi2/B4.py
@@ -1,8 +1,5 @@
-aList = list(map(int, input("Nhap mang: ").split())) 
-k = int(input("Nhap k: "))
-result=0
-for i in range(len(aList)):
-    for j in range(i+1,len(aList)):
-        if aList[i]+aList[j]==k:
-            result+=1
-print("result = ",result)
+aList = list(map(int, input().split())) 
+k = int(input())
+result=sum( aList[i+1:].count(k-aList[i]) for i in range(len(aList)))
+
+print(result)
--- Buoi2/B5.py
+++ Buoi2/B5.py
@@ -1,10 +1,5 @@
-a = int(input("Nhap a: "))
-while a>9:
-    b=a
-    a=0
-    while (b>0):
-        a+=b%10
-        b//=10
-    # print(a)
+a = str(input())
+while (len(a)>1):
+    a = str(sum( int(a[i]) for i in range(len(a))))
 print(a)
     
